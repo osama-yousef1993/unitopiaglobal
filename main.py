750f53aa23af9879c82c6c2f5d3a65bfef730670
osama-yousef1993 <Osama.yousef@optimumpartners.co>
2025-09-22 00:14:04 +0300

    fix issue

--- main.py
+++ main.py
@@ -139,5 +139,5 @@ def netherlands():
 #  Destinations | Services | Contact
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=3000, debug=True)
-    # app.run()
+    # app.run(host="127.0.0.1", port=3000, debug=True)
+    app.run()
